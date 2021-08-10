129ce9d42542c29edc4f05f91df1493c72da6fb0
Hugo Costa <costarafaelhugo@gmail.com>
2021-08-10 13:31:11 -0300

    I add one dice and now 2 dices are throw when the row button is tapped

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -30,11 +30,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
+    lateinit var diceImage1: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
         // Get the Button view from the layout and assign a click
         // listener to it.
         val rollButton: Button = findViewById(R.id.roll_button)
@@ -48,10 +50,9 @@ class MainActivity : AppCompatActivity() {
         // Toast.makeText(this, "button clicked",
         //  Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
-
+        val randomInt1 = (1..6).random()
         var diceImage: ImageView = findViewById(R.id.dice_image)
-
-
+        var diceImage1: ImageView = findViewById(R.id.dice_image1)
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -60,6 +61,16 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+
+        val drawableResource1 = when (randomInt1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
         dice_image.setImageResource(drawableResource)
+        dice_image1.setImageResource(drawableResource1)
     }
 }
